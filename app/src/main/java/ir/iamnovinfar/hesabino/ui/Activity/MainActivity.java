5a7d737f7c63dc374fe773c71a9cbb1495ef14ae
AmirMahdi NovinFar HOME <amirnovin80@gmail.com>
2022-08-07 00:18:30 +0430

    Created Hesabino project and craete main fragment ui

---
+++ app/src/main/java/ir/iamnovinfar/hesabino/ui/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package ir.iamnovinfar.hesabino.ui.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import ir.iamnovinfar.hesabino.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
